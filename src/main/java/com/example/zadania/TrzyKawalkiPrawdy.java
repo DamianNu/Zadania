304858faf5a707f048595af9d64d810d9f938d46
DamianNu <damian21v@gmail.com>
2023-04-28 09:33:59 +0200

    dodanie dwuch dodatkowych zadań z modułu 3.3 i 4.5

---
+++ src/main/java/com/example/zadania/TrzyKawalkiPrawdy.java
@@ -0,0 +1,14 @@
+package com.example.zadania;
+
+public class TrzyKawalkiPrawdy {
+    // Zadanie dodatkowe :  Trzy kawałki prawdy
+    public static void main(String[] args) {
+
+        for (int i=0;i<101;i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/example/zadania/WlosNaKworo.java
@@ -0,0 +1,11 @@
+package com.example.zadania;
+
+public class WlosNaKworo {
+    //Zadanie dodatkowe : Dzielimy włos na czworo, bezwarunkowo!
+
+    public static void main(String[] args) {
+        for (int i=4;i<101;i = i+4){
+            System.out.println(i);
+        }
+    }
+}
