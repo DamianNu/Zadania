dd824c39e26749138e7d5a859192f0350e167324
DamianNu <damian21v@gmail.com>
2023-04-28 09:14:17 +0200

    dodanie algorytmu euklidesa

---
+++ src/main/java/com/example/zadania/Algorithm.java
@@ -0,0 +1,35 @@
+package com.example.zadania;
+
+public class Algorithm {
+    private int a;
+    private int b;
+
+    public Algorithm(){
+        this.a = a;
+        this.b = b;
+    }
+    public int EuklidesAlgorithm(int a,int b){
+        int a1 = a;
+        int b1 = b;
+        int result = 0;
+            while (a!=b){
+                if (a>b){
+                    a = a-b;
+                } else {
+                    b = b-a;
+                }
+            }
+
+        result = a;
+        System.out.println("The greatest common divisor of numbers: " + a1 + " i " + b1 + " is: " + result);
+        return result;
+    }
+
+}
+
+class ProgramA {
+    public static void main(String[] args) {
+        Algorithm algorithm = new Algorithm();
+        algorithm.EuklidesAlgorithm(412692,58956);
+    }
+}
--- src/main/java/com/example/zadania/EuklidesAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.example.zadania;
-
-public class EuklidesAlgorithm {
-
-}
